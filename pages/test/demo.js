a8be646145f431f23ddbe522080ba9e921a12788
smilecamo <1191126635@qq.com>
2019-08-09 16:10:39 +0800

    改动

--- pages/test/demo.js
+++ pages/test/demo.js
@@ -1,19 +1,18 @@
 /*
- * @Author: Yang 
- * @Date: 2019-08-09 15:07:48 
+ * @Author: Yang
+ * @Date: 2019-08-09 15:07:48
  * @Last Modified by: Yang
- * @Last Modified time: 2019-08-09 15:08:12
+ * @Last Modified time: 2019-08-09 15:39:22
  */
 
-
 import { Button } from 'antd';
+import Link from 'next/link';
 import { withRouter } from 'next/router';
-const Test = ({router}) => {
-
+const Test = ({ router }) => {
   return (
-    <Button type="default">
-      主页{router.query.id}
-    </Button>
+    <Link href="/">
+      <Button type="default">主页{router.query.id}</Button>
+    </Link>
   );
 };
 export default withRouter(Test);
--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 
 const app = next({ dev });
+const server = new Koa();
 const router = new Router();
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = new Koa();
   server.use(async (ctx, next) => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
